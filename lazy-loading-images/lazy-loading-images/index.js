bd3dee40ca8ff43a1a1705ed0eac46ff96151728
NEOTSO <neo.tso@outlook.com>
2021-02-25 08:10:28 +0800

    update lazy loading images

--- lazy-loading-images/lazy-loading-images/index.js
+++ lazy-loading-images/lazy-loading-images/index.js
@@ -3,18 +3,15 @@ window.onload = function () {
     const imgArr = document.querySelectorAll(".lazy-load");
     const lazyload = () => {
         [].forEach.call(imgArr, function (item, index) {
-            if (item.dataset.src && !item.getAttribute("loading")) {
+            if (item.dataset.src) {
                 const { top, bottom } = item.getBoundingClientRect();
                 if (bottom > 0 && top < innerHeight) {
-                    console.log(index + " loading...");
                     const img = new Image();
                     img.onload = () => {
-                        item.src = item.dataset.src;
-                        item.removeAttribute("data-src");
-                        item.removeAttribute("loading");
+                        item.src = img.src;
                     };
                     img.src = item.dataset.src;
-                    item.setAttribute("loading", true);
+                    item.removeAttribute("data-src");
                 }
             }
         });
